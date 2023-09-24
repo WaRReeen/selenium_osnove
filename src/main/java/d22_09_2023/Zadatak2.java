27d8042d7d2a5001dc2ae5103a40fbadae85c2ec
Ratko Repic <waren.taily@gmail.com>
2023-09-24 15:15:54 +0200

    Opening Chrome pages(selenium first excercise)

---
+++ src/main/java/d22_09_2023/Zadatak2.java
@@ -0,0 +1,37 @@
+package d22_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Zadatak2 {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+
+        String[] urls = {
+                "https://www.google.com/",
+                "https://www.facebook.com/",
+                "https://www.youtube.com/",
+                "https://www.ebay.com/",
+                "https://www.katalon.com/"
+        };
+
+        for (int i = 0; i < urls.length; i++) {
+            String url = urls[i];
+
+
+
+            driver.get(url);
+            Thread.sleep(1000);
+
+            driver.quit();
+        }
+    }
+}
+
+
+
+
